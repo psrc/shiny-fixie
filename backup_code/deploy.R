0a86042b1c0c76a1591e5488637e7c917c9c960c
Joanne <71201494+Joanneylin@users.noreply.github.com>
2025-05-23 17:05:13 -0700

    code cleanup

--- backup_code/deploy.R
+++
@@ -1,7 +0,0 @@
-library(rsconnect)
-
-# official version ----
-
-deployApp(account = 'psrcwa',
-         appName = 'dashboard-name', # name of your app in the url https://psrcwa.shinyapps.io/dashboard-name
-         appTitle = 'Dashboard Window Title') # name that will be displayed in the window title
