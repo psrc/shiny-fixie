ee08bbcd96267fddf62bb9ec18dde7d5a070e1f3
Joanne <71201494+Joanneylin@users.noreply.github.com>
2025-10-30 10:05:56 -0700

    reorganize main_page script

--- modules/1-main_page.R
+++ modules/1-main_page.R
@@ -6,23 +6,44 @@ edit_interface_ui <- function(id) {
   )
 }
 
-#' HTS data editor user interface
+# HTS data editor user interface
 edit_interface_server <- function(id, selected_error_type) {
   moduleServer(id, function(input, output, session){
     ns <- session$ns
+    
+    rval <- reactiveValues(edit_dt = NULL)
+    observe({
+      # person trip table from database
+      rval$edit_dt <- get_data(person_id = personID(), order_by=c("person_id", "tripnum"))
+    })
+    
+    selected_row_recid <- reactive({ rval$edit_dt[input$thetable_rows_selected, "recid"] })
+    
     
     # person control panel ----
     
     personID <- person_panel_server("panel-person", selected_error_type=reactive(selected_error_type()))
     
+    # data cleaning tools ----
+    
+    # button to add new trip
+    modal_new_trip_server("button_new", 
+                          selected_recid = reactive(selected_row_recid()))
+    # activate Edit Trip modal
+    modal_edit_trip_server("button_edit", 
+                           selected_recid = reactive(selected_row_recid()))
+    # button to delete trip
+    modal_delete_trip_server("button_delete", 
+                             selected_recid = reactive(selected_row_recid()))
+    # link trips interface
+    modal_link_trips_server("button_link", 
+                            selected_recid = reactive(selected_row_recid()))
+    # unlink trip interface
+    modal_unlink_trip_server("button_unlink", 
+                             selected_recid = reactive(selected_row_recid()))
+    
+    
     # the trip table ----
-    
-    # person data from database
-    rval <- reactiveValues(edit_dt = NULL)
-    observe({
-      rval$edit_dt <- get_data(person_id = personID(), order_by=c("person_id", "tripnum"))
-    })
-    
     output$thetable <- DT::renderDataTable(
       
       rval$edit_dt %>% 
@@ -50,23 +71,7 @@ edit_interface_server <- function(id, selected_error_type) {
       
     })  
     
-    # data cleaning tools ----
-    
-    selected_row_recid <- reactive({ rval$edit_dt[input$thetable_rows_selected, "recid"] })
-    
-    ## button to add new trip
-    modal_new_trip_server("button_new", selected_recid = reactive(selected_row_recid()))
-    ## activate Edit Trip modal
-    modal_edit_trip_server("button_edit", selected_recid = reactive(selected_row_recid()))
-    ## button to delete trip
-    modal_delete_trip_server("button_delete", selected_recid = reactive(selected_row_recid()))
-    ## link trips interface
-    modal_link_trips_server("button_link", selected_recid = reactive(selected_row_recid()))
-    ## unlink trip interface
-    modal_unlink_trip_server("button_unlink", selected_recid = reactive(selected_row_recid()))
-    
     # platform layout ----
-    
     output$editplatform <- renderUI({
       tagList(
         fluidRow(#class = "page-format",
